a95ffe0e1dc0b9a9c5443d34c034aef865359d54
alamin <amin.se.aiub@gmail.com>
2024-11-12 15:46:06 +0600

    into php

---
+++ php/test.php
@@ -0,0 +1,52 @@
+<?php
+
+    // $name = "alamin";
+    // $id = 12;
+    // $cgpa = 3.5;
+    // $status = true;
+
+    // $std = array('alamin', 12, 3.5, 'alamin@aiub.edu');
+    // $std[0];
+    // $std = ['alamin', 12, 3.5, 'alamin@aiub.edu'];
+
+    // $stds = [
+    //     ['alamin', 12, 3.5, 'alamin@aiub.edu'],
+    //     ['alamin', 12, 3.5, 'alamin@aiub.edu'],
+    //     ['alamin', 12, 3.5, 'alamin@aiub.edu']
+    // ];
+    // $stds[1][3];
+
+    // $std = ['name'=>'alamin', 'id'=>12, 'cgpa'=>3.5, 'email'=>'alamin@aiub.edu'];
+    // $std['email'];
+    // $stds= [
+    //     's1'=>['name'=>'alamin', 'id'=>12, 'cgpa'=>3.5, 'email'=>'alamin@aiub.edu'],
+    //     's2'=>['name'=>'alamin', 'id'=>12, 'cgpa'=>3.5, 'email'=>'abc@aiub.edu'],
+    //     's3'=>['name'=>'alamin', 'id'=>12, 'cgpa'=>3.5, 'email'=>'xyz@aiub.edu']
+    // ];
+
+    // $stds['s2']['email'];
+
+    //echo "Test PHP ".$name;
+    //print("hello PHP". " test");
+
+    // function sum($a=10, $b=20){
+    //     return $a+$b;
+    // }
+
+    // echo sum(20);
+
+    for($i=0; $i<10; $i++){
+        echo "Hello PHP<br>";
+    }
+
+    // foreach($std as $s){
+    //     echo $s;
+    // }
+
+    // if(10 === "10"){
+    //     echo "true";
+    // }else{
+    //     echo "false";
+    // }
+
+?>
